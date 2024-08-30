143f6898e56f0ad292e947bf4d7142e3b7b5544f
bhaskar <baruahbhaskar10@gmail.com>
2024-08-30 23:42:34 +0530

    Add REDUX toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,77 +1,80 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
-const SignIn = () => {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
-    // console.log(e.target.value);
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
-  // console.log(formData);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all Fields");
+      return dispatch(signInFailure("Please fill all the fields"));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        {/*LEFT */}
+        {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Bhaskar's
+              Sahand's
             </span>
             Blog
           </Link>
-          <p className="tewxt-sm mt-5">
-            This is a demo project.You can sgn ip with your email and password
-            Or use Google
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign in with your email and password
+            or with Google.
           </p>
         </div>
-        {/*RIGHT */}
+        {/* right */}
+
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-            <div className="">
+            <div>
               <Label value="Your email" />
               <TextInput
                 type="email"
-                placeholder="email10@gmail.com"
+                placeholder="name@company.com"
                 id="email"
                 onChange={handleChange}
               />
             </div>
-            <div className="">
-              <Label value="Your Password" />
+            <div>
+              <Label value="Your password" />
               <TextInput
                 type="password"
-                placeholder="********"
+                placeholder="**********"
                 id="password"
                 onChange={handleChange}
               />
@@ -83,7 +86,7 @@ const SignIn = () => {
             >
               {loading ? (
                 <>
-                  <Spinner size="sm"></Spinner>
+                  <Spinner size="sm" />
                   <span className="pl-3">Loading...</span>
                 </>
               ) : (
@@ -91,10 +94,9 @@ const SignIn = () => {
               )}
             </Button>
           </form>
-
           <div className="flex gap-2 text-sm mt-5">
-            <span>Don't have an account?</span>
-            <Link to="/sign-in" className="text-blue-500">
+            <span>Dont Have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
               Sign Up
             </Link>
           </div>
@@ -107,5 +109,4 @@ const SignIn = () => {
       </div>
     </div>
   );
-};
-export default SignIn;
+}
