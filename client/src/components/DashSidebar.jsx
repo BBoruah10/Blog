4582192417db86624ea142e8b6fa4256ae954947
bhaskar <baruahbhaskar10@gmail.com>
2024-09-05 01:06:01 +0530

    Complete User Image Upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -24,6 +24,7 @@ export const DashSidebar = () => {
               icon={HiUser}
               label={"User"}
               labelColor="dark"
+              as="div"
             >
               Profile
             </Sidebar.Item>
